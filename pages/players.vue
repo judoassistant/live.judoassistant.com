eca4f4cfc63968ac337abd4aa9b5e18e493b001d
Svend Christian Svendsen <svendcsvendsen@gmail.com>
2019-03-27 16:39:29 +0100

    Setup player column sorting

--- pages/players.vue
+++ pages/players.vue
@@ -1,7 +1,34 @@
 <template>
   <section class="section">
     <h1 class="title">Players {{counter}}</h1>
-    <b-table paginated :per-page=15 :data="players" :columns="columns"></b-table>
+    <b-table paginated :per-page=15 :data="players" default-sort="last_name">
+      <template v-slot:default="props">
+        <b-table-column sortable field="first_name" label="First Name">
+          {{ props.row.first_name}}
+        </b-table-column>
+        <b-table-column sortable field="last_name" label="Last Name">
+          {{ props.row.last_name}}
+        </b-table-column>
+        <b-table-column sortable field="grade" label="Grade">
+          {{ props.row.grade}}
+        </b-table-column>
+        <b-table-column sortable field="club" label="Club">
+          {{ props.row.club}}
+        </b-table-column>
+      </template>
+
+      <template v-slot:empty>
+        <section class="section">
+          <div class="content has-text-grey has-text-centered">
+            <p>
+              <b-icon icon="account-multiple" size="is-large">
+              </b-icon>
+            </p>
+            <p>There are no players in the tournament</p>
+          </div>
+        </section>
+      </template>
+    </b-table>
   </section>
 </template>
 
