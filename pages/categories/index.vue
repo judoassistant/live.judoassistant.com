793992adb31e85c0f87ba1dc8060ac07b882da40
Svend Christian Svendsen <svendcsvendsen@gmail.com>
2019-03-27 17:08:31 +0100

    Update categories table

--- pages/categories/index.vue
+++ pages/categories/index.vue
@@ -1,7 +1,31 @@
 <template>
   <section class="section">
     <h1 class="title">Categories</h1>
-    <b-table :data="data" :columns="columns"></b-table>
+    <b-table paginated :per-page=15 :data="categories" default-sort="name">
+      <template v-slot:default="props">
+        <b-table-column sortable field="name" label="Name">
+          {{ props.row.name}}
+        </b-table-column>
+        <b-table-column sortable field="state" label="State">
+          {{ props.row.state}}
+        </b-table-column>
+        <b-table-column sortable field="playerCount" label="Number of Players">
+          {{ props.row.playerCount}}
+        </b-table-column>
+      </template>
+
+      <template v-slot:empty>
+        <section class="section">
+          <div class="content has-text-grey has-text-centered">
+            <p>
+              <b-icon icon="account-multiple" size="is-large">
+              </b-icon>
+            </p>
+            <p>There are no categories in the tournament</p>
+          </div>
+        </section>
+      </template>
+    </b-table>
   </section>
 </template>
 
@@ -10,20 +34,13 @@
 
 export default {
   name: 'HomePage',
+  computed: {
+    categories() {
+      return this.$store.state.categories
+    },
+  },
   data() {
     return {
-      data: [
-        { 'id': 1, 'name': 'Drenge A -45', 'state': 'finished', 'playerCount': 4},
-        { 'id': 2, 'name': 'Drenge A -50', 'state': 'not_started', 'playerCount': 7},
-        { 'id': 3, 'name': 'Drenge A -55', 'state': 'not_started', 'playerCount': 3},
-        { 'id': 4, 'name': 'Drenge A -66', 'state': 'in_progress', 'playerCount': 5},
-        { 'id': 5, 'name': 'Drenge A -81', 'state': 'in_progress', 'playerCount': 6},
-        { 'id': 6, 'name': 'Piger A -48', 'state': 'finished', 'playerCount': 3},
-        { 'id': 7, 'name': 'Piger A -52', 'state': 'not_started', 'playerCount': 5},
-        { 'id': 8, 'name': 'Piger A -57', 'state': 'not_started', 'playerCount': 6},
-        { 'id': 9, 'name': 'Piger B -48', 'state': 'in_progress', 'playerCount': 5},
-        { 'id': 10, 'name': 'Piger C -35', 'state': 'in_progress', 'playerCount': 2},
-      ],
       columns: [
         {
           field: 'name',
--- store/index.js
+++ store/index.js
@@ -22,6 +22,18 @@ export const state = () => ({
     { 'id': 19, 'first_name': 'Clarence', 'last_name': 'Flores', 'gender': 'Male', 'grade': '5. kyu', 'club': 'Aarhus Judo Klub'},
     { 'id': 20, 'first_name': 'Anne', 'last_name': 'Lee', 'gender': 'Female', 'grade': '5. kyu', 'club': 'Aarhus Judo Klub'},
   ],
+  categories: [
+    { 'id': 1, 'name': 'Drenge A -45', 'state': 'finished', 'playerCount': 4},
+    { 'id': 2, 'name': 'Drenge A -50', 'state': 'not_started', 'playerCount': 7},
+    { 'id': 3, 'name': 'Drenge A -55', 'state': 'not_started', 'playerCount': 3},
+    { 'id': 4, 'name': 'Drenge A -66', 'state': 'in_progress', 'playerCount': 5},
+    { 'id': 5, 'name': 'Drenge A -81', 'state': 'in_progress', 'playerCount': 6},
+    { 'id': 6, 'name': 'Piger A -48', 'state': 'finished', 'playerCount': 3},
+    { 'id': 7, 'name': 'Piger A -52', 'state': 'not_started', 'playerCount': 5},
+    { 'id': 8, 'name': 'Piger A -57', 'state': 'not_started', 'playerCount': 6},
+    { 'id': 9, 'name': 'Piger B -48', 'state': 'in_progress', 'playerCount': 5},
+    { 'id': 10, 'name': 'Piger C -35', 'state': 'in_progress', 'playerCount': 2},
+  ],
 })
 
 export const mutations = {
