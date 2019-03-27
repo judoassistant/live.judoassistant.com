e14ec3a336b9d21eb03db2e95b20ee78a31c247d
Svend Christian Svendsen <svendcsvendsen@gmail.com>
2019-03-27 17:31:03 +0100

    Move navigation into navbar

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,52 +1,30 @@
 <template>
   <div>
-    <nav
-      class="navbar header has-shadow is-primary"
-      role="navigation"
-      aria-label="main navigation"
-    >
+    <nav class="navbar header has-shadow is-primary" role="navigation" aria-label="main navigation">
       <div class="navbar-brand">
-        <a
-          class="navbar-item"
-          href="/"
-        >
-          <img
-            src="~assets/logo.svg"
-            alt="JudoAssistant"
-            height="28"
-          >
-        </a>
+        <nuxt-link class="navbar-item" to="/" exact-active-class="is-active">
+          <img src="~assets/logo.svg" alt="JudoAssistant" height="28" />
+        </nuxt-link>
+        <nuxt-link to="/players" class="navbar-item" exact-active-class="is-active">
+          Players
+        </nuxt-link>
+        <nuxt-link to="/categories" class="navbar-item" exact-active-class="is-active">
+          Categories
+        </nuxt-link>
+        <nuxt-link to="/tatamis" class="navbar-item" exact-active-class="is-active">
+          Tatamis
+        </nuxt-link>
 
-        <div class="navbar-burger">
-          <span />
-          <span />
-          <span />
-        </div>
+        <!-- <div class="navbar-burger"> -->
+        <!--   <span /> -->
+        <!--   <span /> -->
+        <!--   <span /> -->
+        <!-- </div> -->
       </div>
     </nav>
 
-    <section class="main-content columns">
-
-      <aside class="column is-2 section">
-        <ul class="menu-list">
-          <li
-            v-for="(item, key) of items"
-            :key="key"
-          >
-            <nuxt-link
-              :to="item.to"
-              exact-active-class="is-active"
-            >
-              <b-icon :icon="item.icon" /> {{ item.title }}
-            </nuxt-link>
-          </li>
-        </ul>
-      </aside>
-
-      <div class="container column is-10">
-        <nuxt />
-      </div>
-
+    <section class="main-content">
+      <nuxt />
     </section>
   </div>
 </template>
