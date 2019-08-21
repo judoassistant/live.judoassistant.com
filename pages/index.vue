e3d032c73fe58a3e98e100baffd52eee2ede2ede
Svend Christian Svendsen <svendcsvendsen@gmail.com>
2019-08-21 14:14:37 +0200

    Add hardcoded front page

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,21 @@
 <template>
   <section class="section">
     <div class="container">
-      <h1 class="title">JudoAssistant</h1>
+      <h1 class="title">Current Tournaments</h1>
+      <table class="table is-striped is-fullwidth">
+        <thead>
+          <th>Tournament</th>
+          <th>Location</th>
+          <th>Date</th>
+        </thead>
+        <tbody>
+          <tr>
+            <td><nuxt-link :to="{ name: 'tournament', params: {tournament: 'bjergkobing' }}">Bjergkøbing Grand Prix</nuxt-link></td>
+            <td>Bjergkøbing</td>
+            <td>24th of August</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   </section>
 </template>
