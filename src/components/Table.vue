afd478185c9a7244d7fbe49f9832a4ff46c3f40c
Svend Christian Svendsen <svendcs@svendcs.com>
2020-11-03 22:10:02 +0100

    Implement MVP Table component

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -1,19 +1,11 @@
 <template>
   <table>
     <tr>
-      <th>Tournament</th>
-      <th>Location</th>
-      <th class="active">Date <span class="mdi mdi-arrow-down"></span></th>
+      <!-- <th class="active">Date <span class="mdi mdi-arrow-down"></span></th> -->
+      <th v-for="field in fields" :key="field.field">{{field.label}}</th>
     </tr>
-    <tr>
-      <td><a href="#">Ungdomsturnering Fredericia</a></td>
-      <td>Fredericia</td>
-      <td>Saturday, October 24, 2020</td>
-    </tr>
-    <tr>
-      <td><a href="#">Ungdomsturnering Korsør</a></td>
-      <td>Korsør</td>
-      <td>Saturday, October 24, 2020</td>
+    <tr v-for="row in data" :key="row.id">
+      <slot v-bind:row="row"></slot>
     </tr>
   </table>
 </template>
@@ -21,29 +13,9 @@
 <script>
 export default {
   name: 'Table',
-  /* props: { */
-  /*   msg: String */
-  /* } */
-  data: function() {
-    return {
-      tournaments: [
-        {
-          name: "Ungdomsturnering Fredericia",
-          loc: "Fredericia",
-          dat: "Saturday, October 24, 2020",
-        },
-        {
-          name: "Ungdomsturnering Korsør",
-          loc: "Korsør",
-          dat: "Saturday, October 24, 2020",
-        },
-        {
-          name: "Ungdomsturnering Skive",
-          loc: "Skive",
-          dat: "Saturday, October 17, 2020",
-        },
-      ]
-    }
+  props: {
+    data: Array,
+    fields: Array,
   }
 }
 </script>
@@ -53,6 +25,7 @@ export default {
 
   table th.active {
     border-bottom: 1px solid $color3;
+    color: $color1;
   }
 </style>
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,9 +2,12 @@
   <main>
     <h1>Tournament Overview</h1>
     <h2>Upcoming Tournaments</h2>
-    <Table/>
+    <Table :data="data" :fields="fields" v-slot="props">
+      <td><a href="#">{{ props.row.name }}</a></td>
+      <td>{{ props.row.loc }}</td>
+      <td>{{ props.row.dat }}</td>
+    </Table>
     <h2>Previous Tournaments</h2>
-    <Table/>
   </main>
 </template>
 
@@ -12,8 +15,35 @@
 import Table from '@/components/Table.vue'
 
 export default {
-  components: {
-    Table
+  components: { Table },
+  data: function() {
+    return {
+      data: [
+        {
+          id: 1,
+          name: "Ungdomsturnering Fredericia",
+          loc: "Fredericia",
+          dat: "Saturday, October 24, 2020",
+        },
+        {
+          id: 2,
+          name: "Ungdomsturnering Korsør",
+          loc: "Korsør",
+          dat: "Saturday, October 24, 2020",
+        },
+        {
+          id: 3,
+          name: "Ungdomsturnering Skive",
+          loc: "Skive",
+          dat: "Saturday, October 17, 2020",
+        },
+      ],
+      fields: [
+        { field: 'name', label: 'Name'},
+        { field: 'loc', label: 'Location'},
+        { field: 'dat', label: 'Date'},
+      ]
+    }
   },
 }
 </script>
