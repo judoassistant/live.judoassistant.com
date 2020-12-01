0f665c01a9dec2f5e4e678770b96435921ef8022
Svend Christian Svendsen <svendcs@svendcs.com>
2020-12-01 20:19:53 +0100

    Add temporary connection state indicators

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,25 @@
 <template>
   <MainMenu />
-  <router-view/>
+  <template v-if="connectionState == 'NOT_CONNECTED'">
+    <p style='text-align: center;'>not connected....</p>
+  </template>
+  <template v-if="connectionState == 'CONNECTING'">
+    <p style='text-align: center;'>not connected....</p>
+  </template>
+  <router-view v-if="connectionState == 'CONNECTED'"/>
 </template>
 
 <script>
-// @ is an alias to /src
 import MainMenu from '@/components/MainMenu.vue'
+import { mapState } from 'vuex'
 
 export default {
   name: 'App',
+  computed: {
+    ...mapState({
+      connectionState: state => state.connectionState
+    }),
+  },
   created: function() {
     this.$store.dispatch('connect')
   },
