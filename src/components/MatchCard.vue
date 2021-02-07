0aa7a27cf8b7a552ba2b0db4c7d98e314c468292
Svend Christian Svendsen <svendcs@svendcs.com>
2021-02-07 20:21:15 +0100

    Add MatchCard scaffolding

---
+++ src/components/MatchCard.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>match card</div>
+</template>
+
+<script>
+export default {
+  name: 'MatchCard',
+  props: {
+    matchId: int
+  },
+}
+</script>
+
+<style scoped lang="scss">
+  @import "../styles/colors.module.scss";
+
+</style>
+
