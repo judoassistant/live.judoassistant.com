0cdffbe883c5fe0dbf3b46d0bfa28482c42b37d1
Svend Christian Svendsen <svendcs@svendcs.com>
2021-03-13 16:50:41 +0100

    Add markup for PaginiationControls

--- src/components/PaginationControls.vue
+++ src/components/PaginationControls.vue
@@ -1,5 +1,14 @@
 <template>
-  <p>Controls go here</p>
+  <ul class="pagination">
+    <li class="pagination-previous"><a href="#">&lt;</a></li>
+    <li class="pagination-number"><a href="#">1</a></li>
+    <li class="pagination-dots">...</li>
+    <li class="pagination-number"><a href="#">4</a></li>
+    <li class="pagination-number"><a href="#">5</a></li>
+    <li class="pagination-number"><a href="#">6</a></li>
+    <li class="pagination-number"><a href="#">7</a></li>
+    <li class="pagination-next"><a href="#">&gt;</a></li>
+  </ul>
 </template>
 
 <script>
@@ -14,6 +23,46 @@ export default {
 
 <style scoped lang="scss">
   @import "../styles/colors.module.scss";
+
+  $long-distance: .8em;
+  $short-distance: .4em;
+
+  .pagination {
+    list-style-type: none;
+    text-align: right;
+    margin: 20px 0;
+  }
+
+  .pagination li {
+    display: inline;
+  }
+
+  .pagination a, .pagination-dots {
+    color: $color4;
+  }
+
+  .pagination a:hover {
+    text-decoration: none;
+    color: $color1;
+  }
+
+  .pagination-previous {
+    margin-right: $long-distance;
+  }
+
+  .pagination-number {
+    margin-left: $short-distance;
+    margin-right: $short-distance;
+  }
+
+  .pagination-dots {
+    margin-left: $long-distance;
+    margin-right: $long-distance;
+  }
+
+  .pagination-next {
+    margin-left: $long-distance;
+  }
 
 </style>
 
