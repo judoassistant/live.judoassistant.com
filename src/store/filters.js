862b6f73504f0c081e37a358e591c0c256c6d579
Svend Christian Svendsen <svendcs@svendcs.com>
2021-03-13 21:03:47 +0100

    Fix dateFilter

--- src/store/filters.js
+++ src/store/filters.js
@@ -112,7 +112,7 @@ export function blockStatusFilter(status) {
 
 export function dateFilter(date) {
   const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-  const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
-  return days[date.getDay()-1] + ', ' + months[date.getMonth()-1] + ' ' + date.getDate() + ', ' + date.getFullYear();
+  const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+  return days[date.getDay()] + ', ' + months[date.getMonth()] + ' ' + date.getDate() + ', ' + date.getFullYear();
 }
 
