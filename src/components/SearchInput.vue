4807e5795e95430c4c020ad9bd148c404fa63c6a
Simon Kruse <sik@capturi.com>
2021-11-07 12:53:16 +0100

    feat: added figther/player search

---
+++ src/components/SearchInput.vue
@@ -0,0 +1,48 @@
+<template>
+    <input :id="id" :type="type" :value="modelValue" @input="updateInput" class="input" />
+</template>
+
+<script>
+export default {
+    name: "SearchInput",
+    props: {
+        id: {
+            type: String,
+            default: "",
+        },
+        label: {
+            type: String,
+            default: "",
+        },
+        modelValue: {
+            type: [String, Number],
+            default: "",
+        },
+        type: {
+            type: String,
+            default: "text",
+        }
+    },
+
+    methods: {
+        updateInput(event) {
+            this.$emit("update:modelValue", event.target.value);
+        }
+    }
+}
+</script>
+
+<style scoped lang="scss">
+@import "../styles/constants.module.scss";
+
+.input {
+    padding: 10px 20px;
+    background-color: white;
+    border-radius: 3px;
+    border: $color2 1px solid;
+
+    width: 100%;
+    box-sizing: border-box;
+    border-radius: 110px;
+}
+</style>
--- src/views/TournamentPlayers.vue
+++ src/views/TournamentPlayers.vue
@@ -1,30 +1,36 @@
 <template>
-  <h2>Players</h2>
-  <InfoText v-if="players.length == 0">This tournament has no players yet.</InfoText>
-  <Table v-if="players.length > 0" :headers="headers" :rows="players" v-slot="props">
-    <TableColumn>
-      <router-link class="menu-item" :to="{ name: 'tournament-player', params: { tournament: this.$route.params.tournament, playerId: props.row.id }}">{{ props.row.firstName }}</router-link>
-    </TableColumn>
-    <TableColumn>
-      {{ props.row.lastName }}
-    </TableColumn>
-    <TableColumn>
-      {{ props.row.club }}
-    </TableColumn>
-  </Table>
+  <InfoText v-if="!hasPlayers">This tournament has no players yet.</InfoText>
+
+  <template v-if="hasPlayers">
+    <h2>Players</h2>
+
+    <Input v-model="searchTerm" placeholder="Search" />
+    <Table :headers="headers" :rows="players" v-slot="props">
+      <TableColumn>
+        <router-link
+          :to="{ name: 'tournament-player', params: { tournament: this.$route.params.tournament, playerId: props.row.id } }"
+          class="menu-item"
+        >{{ props.row.firstName }}</router-link>
+      </TableColumn>
+      <TableColumn>{{ props.row.lastName }}</TableColumn>
+      <TableColumn>{{ props.row.club }}</TableColumn>
+    </Table>
+    <InfoText v-if="players.length === 0">No players found</InfoText>
+  </template>
 </template>
 
 <script>
-/* <TabItem :label="'Tatami ' + (index + 1)" :key="index" v-for="(matches, index) of tatamiMatches"> */
 import Table from '@/components/Table.vue'
 import TableColumn from '@/components/TableColumn.vue'
 import InfoText from '@/components/InfoText.vue'
+import Input from '@/components/Input.vue'
 import { mapState } from 'vuex'
 
 export default {
-  components: { Table, TableColumn, InfoText },
+  components: { Table, TableColumn, InfoText, Input },
   data() {
     return {
+      searchTerm: "",
       headers: [
         { 'field': 'firstName', 'label': 'First Name', 'sortable': true },
         { 'field': 'lastName', 'label': 'Last Name', 'sortable': true },
@@ -34,9 +40,22 @@ export default {
   },
   computed: {
     ...mapState({
-      players: state => Array.from(state.players.values()),
+      hasPlayers: state => state.players.size >= 1,
+      players: function (state) {
+        const searchTerm = this.searchTerm
+        return Array.from(state.players.values()).filter(p => {
+          if (searchTerm === "") return true
+
+          //People doesnt always know the fightes full name
+          //So if just one of the names mathces, we are happy
+          const names = `${p.firstName} ${p.lastName}`.toLowerCase().split(" ")
+          const words = searchTerm.split(" ")
+
+          return names.some(n => words.some(w => w && n.startsWith(w.toLowerCase())))
+        })
+      },
     }),
-  },
+  }
 }
 </script>
 
