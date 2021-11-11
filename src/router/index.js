7392d0771e85556a47c289f8fc2c90285334bd5b
Svend Christian Svendsen <svendcs@svendcs.com>
2021-11-11 21:22:31 +0100

    Switch to vue html history mode

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import Home from '@/views/Home.vue'
 import Tournament from '@/views/Tournament.vue'
 import TournamentHome from '@/views/TournamentHome.vue'
@@ -53,7 +53,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes,
 })
 
